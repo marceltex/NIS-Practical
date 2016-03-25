f1d9bec463934d81aae817d19ecd1d4d5db3836a
Marcel Teixeira <marceltex@gmail.com>
2016-03-25 23:56:13 +0200

    Created project in Netbeans

---
+++ src/nispractical/NISPractical.java
@@ -0,0 +1,16 @@
+package nispractical;
+
+/**
+ *
+ * @author Marcel
+ */
+public class NISPractical {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
