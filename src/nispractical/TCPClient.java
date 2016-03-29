30bf2747e902d1cb83d2627b5058108716ed2991
Marcel Teixeira <marceltex@gmail.com>
2016-03-29 20:11:17 +0200

    Implemented the TCP Client as shown at this website: https://systembash.com/a-simple-java-tcp-server-and-tcp-client/

--- src/nispractical/TCPClient.java
+++ src/nispractical/TCPClient.java
@@ -1,9 +1,32 @@
 package nispractical;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
  * @author Marcel Teixeira
  */
 public class TCPClient {
-    
+
+    /**
+     * @param args the command line arguments
+     * @throws Exception 
+     */
+    public static void main(String[] args) throws Exception {
+        String sentence;
+        String modifiedSentence;
+        Scanner inFromUser = new Scanner(System.in);
+        Socket clientSocket = new Socket("localhost", 6789);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        sentence = inFromUser.nextLine();
+        outToServer.writeBytes(sentence + '\n');
+        modifiedSentence = inFromServer.readLine();
+        System.out.println("FROM SERVER: " + modifiedSentence);
+        clientSocket.close();
+    }
 }
