1044e81b8f71f73333dbe69df77aca1e25df8844
Marcel Teixeira <marceltex@gmail.com>
2016-03-29 18:35:47 +0200

    Renamed 'NISPractical.java' to 'TCPServer.java'

---
+++ src/nispractical/TCPServer.java
@@ -0,0 +1,15 @@
+package nispractical;
+
+/**
+ *
+ * @author Marcel Teixeira
+ */
+public class TCPServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    } 
+}
