7720bc2eb07e3728281acd49495f8e1d4e221c95
Marcel Teixeira <marceltex@gmail.com>
2016-03-29 18:56:17 +0200

    Implemented the TCP Server as shown at this website: https://systembash.com/a-simple-java-tcp-server-and-tcp-client/

--- src/nispractical/TCPServer.java
+++ src/nispractical/TCPServer.java
@@ -1,5 +1,11 @@
 package nispractical;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author Marcel Teixeira
@@ -9,7 +15,20 @@ public class TCPServer {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-       
-    } 
+    public static void main(String[] args) throws Exception {
+        String clientSentence;
+        String capitalisedSentence;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient
+                    = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            System.out.println("Received: " + clientSentence);
+            capitalisedSentence = clientSentence.toUpperCase();
+            outToClient.writeBytes(capitalisedSentence);
+        }
+    }
 }
