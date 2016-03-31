2aa62fcdc63f5299a8d55fafc29a1e2a7c0fba34
Marcel Teixeira <marceltex@gmail.com>
2016-04-01 01:43:25 +0200

    Added separate try/catches to handle exceptions more sensibly. Added a description for the Java class.

--- src/nispractical/TCPServer.java
+++ src/nispractical/TCPServer.java
@@ -1,17 +1,21 @@
 package nispractical;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- *
+ * A TCP Server that will decrypt a message that was encrypted by the client 
+ * using the PGP protocol.
+ * 
  * @author Marcel Teixeira
+ * @version 0.4
  */
 public class TCPServer {
-    
+
     public static final int PORT = 2222;
 
     /**
@@ -21,22 +25,39 @@ public class TCPServer {
         String clientMessage;
         String capitalisedMessage;
 
-        ServerSocket serverSocket = new ServerSocket(PORT);
+        ServerSocket serverSocket = null;
+        BufferedReader inFromClient = null;
+        PrintStream outToClient = null;
+        Socket clientSocket = null;
 
-        System.out.println("The server has started. To stop it press <CTRL><C>.");
+        // Open a server socket on port 2222
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("The server has started using Port Number: "
+                    + PORT + ". To stop it press <CTRL><C>.");
+        } catch (IOException e) {
+            System.err.println("Failed to start the server on Port Number: "
+                    + PORT + ". Error message produced: " + e);
+        }
 
-        Socket clientSocket = serverSocket.accept();
+        // Create a socket object from the ServerSocket to listen to and accept
+        // connections. Open input and output streams.
+        try {
+            clientSocket = serverSocket.accept();
 
-        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        PrintStream outToClient = new PrintStream(clientSocket.getOutputStream());
+            inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            outToClient = new PrintStream(clientSocket.getOutputStream());
 
-        while (true) {
-            clientMessage = inFromClient.readLine();
+            while (true) {
+                clientMessage = inFromClient.readLine();
 
-            System.out.println("Received: " + clientMessage);
+                System.out.println("Received: " + clientMessage);
 
-            capitalisedMessage = clientMessage.toUpperCase();
-            outToClient.println(capitalisedMessage);
+                capitalisedMessage = clientMessage.toUpperCase();
+                outToClient.println(capitalisedMessage);
+            }
+        } catch (IOException e) {
+            System.err.println(e);
         }
     }
 }
